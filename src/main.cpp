98f6995ff8a7607f94279e9ba249ed4c7e71a4ea
Piero24 <32714047+Piero24@users.noreply.github.com>
2023-05-29 11:51:52 +0200

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -5,26 +5,45 @@
 
 int main(int argc, char** argv) {
 
-    char in;
-    std::cout << "Press enter to start:" << std::endl;
-    std::cin.get(in);
+    std::vector<cv::Mat> imagesProjected;
+	std::vector<cv::Mat> descriptors;
+	std::vector<std::vector<cv::KeyPoint>> keypoints;
 
-    std::cout << "Task 1 press any key to continue." << std::endl;
-    task1(argc, argv);
+	std::vector<cv::String> fileInFolderVec;
 
-    std::cout << "Task 2 press any key to continue." << std::endl;
-    task2(argc, argv);
+    cv::glob(argv[1], fileInFolderVec, false);
 
-    std::cout << "Task 3 press any key to continue." << std::endl;
-    task3(argc, argv);
+    size_t count = fileInFolderVec.size();
+    for (size_t i = 0; i < count; i++) {
+        if (fileInFolderVec[i].substr(fileInFolderVec[i].size() - 4) == ".jpg" ||
+            fileInFolderVec[i].substr(fileInFolderVec[i].size() - 4) == ".png" ||
+            fileInFolderVec[i].substr(fileInFolderVec[i].size() - 4) == ".bmp") {
 
-    std::cout << "Task 4 press any key to continue." << std::endl;
-    task4(argc, argv);
+            cv::Mat img = cv::imread(fileInFolderVec[i]);
+            imagesProjected.push_back(img);
+        }
+    }
 
-    std::cout << "Task 5 press any key to continue." << std::endl;
-    task5(argc, argv);
+    // char in;
+    // std::cout << "Press enter to start:" << std::endl;
+    // std::cin.get(in);
 
-    std::cout << "Tasks terminated." << std::endl;
+    // std::cout << "Task 1 press any key to continue." << std::endl;
+    // task1(argc, argv);
+
+    // std::cout << "Task 2 press any key to continue." << std::endl;
+    // task2(argc, argv);
+
+    // std::cout << "Task 3 press any key to continue." << std::endl;
+    // task3(argc, argv);
+
+    // std::cout << "Task 4 press any key to continue." << std::endl;
+    // task4(argc, argv);
+
+    // std::cout << "Task 5 press any key to continue." << std::endl;
+    // task5(argc, argv);
+
+    // std::cout << "Tasks terminated." << std::endl;
 
     return 0;
 }
